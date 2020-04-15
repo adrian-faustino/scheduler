d2ec8183d511cd7786e9c59528d67273715ad35a
Adrian Faustino <adrianfaustino@Adrians-MacBook-Pro.local>
2020-04-15 01:42:32 -0400

    Appointmentlist now shows booked timeslots

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
 import './styles.scss';
 import React from 'react'
 
+// components
+import Header from './Header.js';
+import Show from './Show.js';
+import Empty from './Empty.js';
+
 export default function Appointment(props) {
+  const { id, time, interview} = props;
   return (
     <div>
-      <article className="appointment">{props.time}</article>
+      <Header time={time}/>
+      {interview ? <Show {...interview }/> : <Empty />}
     </div>
   )
 }
