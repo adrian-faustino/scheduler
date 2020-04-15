41113630a50eb89949a36b8eac03b4ffb20fa81d
Adrian Faustino <adrianfaustino@Adrians-MacBook-Pro.local>
2020-04-15 02:03:20 -0400

    Added schedule list component from storybook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,84 @@
 import React, { useState } from "react";
-import DayList from "components/DayList";
+
+// styles
 import "components/Application.scss";
 
+// components
+import DayList from "components/DayList";
+import Appointment from "components/Appointment/index.js";
+
+
+//Dummy data
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm"
+  },
+  {
+    id: 4,
+    time: "3pm"
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Adrian-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+];
+
+const defaultDay = "Monday";
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
 export default function Application(props) {
-  const defaultDay = "Monday";
   const [day, setDay] = useState(defaultDay);
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+
+  // Spread appointment data for rendering
+  const appointments_ = appointments.map(appointment => {
+    return (
+      <Appointment key={appointment.id}
+      {...appointment}
+      />
+    );
+  });
+
 
   return (
     <main className="layout">
@@ -45,7 +102,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments_}
       </section>
       
     </main>
