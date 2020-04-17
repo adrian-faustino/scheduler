ef955783a1a1f78fbfc7005ef54c40cdae12093d
Adrian Faustino <adrianfaustino@Adrians-MacBook-Pro.local>
2020-04-17 18:40:17 -0400

    Wrote a custom hook that transitions from frst mode to second mode

---
+++ src/hooks/useVisualMode.test.js
@@ -0,0 +1,40 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import useVisualMode from "hooks/useVisualMode";
+
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
+
+// testing for initialization
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+
+// testing for transition function
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+// testing for back function
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
